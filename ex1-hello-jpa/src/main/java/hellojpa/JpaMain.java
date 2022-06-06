3c21861a9fe09b7ed5345db9acaed24567636ec5
jki503 <jki503@naver.com>
2022-06-06 22:01:26 +0900

    이전 거

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -18,19 +18,10 @@ public class JpaMain {
         tx.begin();
 
         try{
-            //비영속
-
-            // 1. select
-            Member member = em.find(Member.class, 1L);
-
-            System.out.println("======before======");
-
-            // 2. 비영속 상태
-            em.remove(member);
-
-            // 3. 이름 조회
-            System.out.println(member.getName());
-            System.out.println("======after======");
+            System.out.println("===");
+            em.persist(new Member("cc"));
+            em.persist(new Member("dd"));
+            System.out.println("===");
 
             tx.commit();//4. 실제 쿼리 날아가는 시점
         }catch (Exception e){
--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,23 +1,25 @@
 package hellojpa;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Date;
 import java.util.StringJoiner;
 
 @Entity
+@SequenceGenerator(
+        name = "MEMBER_SEQ_GENERATOR",
+        sequenceName = "MEMBER_SEQ", //매핑할 데이터베이스 시퀀스 이름
+        initialValue = 1, allocationSize = 50)
 public class Member {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MEMBER_SEQ_GENERATOR")
     private Long id;
 
     private String name;
 
     protected Member(){}
 
-    public Member(Long id, String name) {
-        this.id = id;
+    public Member(String name) {
         this.name = name;
     }
 
